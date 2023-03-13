e92af5808b0b3ed8dbfc9be5444ebeda8e45da18
yabinmeng <yabinmeng@gmail.com>
2023-03-13 12:59:56 -0500

    Deployment script enhancement with customized "client.conf" support for astra or luna_existing deployment.

--- _bash_utils_/utilities.sh
+++ _bash_utils_/utilities.sh
@@ -197,6 +197,6 @@ outputMsg() {
 ##
 # Check if the sed being used is GNU sed
 isGnuSed() {
-    local gnu_sed=$(sed --version | grep "GNU sed" | wc -l)
+    local gnu_sed=$(sed --version 2>&1 | grep -v 'illegal\|usage\|^\s' | grep "GNU sed" | wc -l)
     echo ${gnu_sed}
 }
